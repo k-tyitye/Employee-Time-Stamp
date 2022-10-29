da7a0a40fe08f8b4bc68df871c159ee652e9b586
k-tyitye <115266106+k-tyitye@users.noreply.github.com>
2022-10-28 20:56:13 -0400

    Add files via upload

---
+++ GuiEmployeeDatabaseTests/DatabaseInteractionTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GuiEmployeeDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiEmployeeDatabase.Tests
+{
+    [TestClass()]
+    public class DatabaseInteractionTests
+    {
+
+        [TestMethod()]
+        public void getEmployeeByIdTestUsingTestDB()
+        {
+            DatabaseInteraction db = new DatabaseInteraction(true);
+
+            Assert.IsTrue(db.getEmployeeById(1));
+        }
+
+        [TestMethod()]
+        public void getEmployeeByIdTestUsingMainDB()
+        {
+            DatabaseInteraction db = new DatabaseInteraction(false);
+
+            Assert.IsTrue(db.getEmployeeById(1));
+        }
+
+        [TestMethod()]
+        public void insertClockInTimeTest()
+        {
+            DatabaseInteraction db = new DatabaseInteraction(false);
+
+            Assert.IsTrue(db.insertClockInTime(1));
+
+        }
+
+        [TestMethod()]
+        public void insertClockOutTimeTest()
+        {
+            DatabaseInteraction db = new DatabaseInteraction(false);
+
+            Assert.IsTrue(db.insertClockOutTime(1));
+        }
+    }
+}
